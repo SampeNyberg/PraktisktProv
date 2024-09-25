9024820ffe4843083a54eb2a06f4837fbfbd6108
samuel <samnyb1109@skola.goteborg.se>
2024-09-25 11:18:04 +0200

    hej hej

---
+++ praktisktprov.py
@@ -0,0 +1,16 @@
+myList = []
+
+myList.append(input("ange dina värden")) 
+
+x = str(myList)
+print(f'{x[2]}')
+
+c = len(x)
+if c < 2:
+    for i in range(11):
+        b = int(x[2])*i
+        print(f'{x[2]} * {i} = {b}')
+elif c > 2 < 5:   
+    for i in range(11):
+        b = int(x[4])*i
+        print(f'{x[4]} * {i} = {b}')
